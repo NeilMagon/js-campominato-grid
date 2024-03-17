323f2558c6f22f6a9ca5ae464aadcd27c52ddee5
NeilMagon <neil.magon97@gmail.com>
2024-03-17 16:39:28 +0100

    feat: add javascript function

--- js/script.js
+++ js/script.js
@@ -5,3 +5,34 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si 
 // colora di azzurro ed emetto un messaggio in console 
 // con il numero della cella cliccata.
+
+const play = document.querySelector("#play");
+
+const grid =  document.querySelector("#grid");
+
+// Quando clicclo su play  creo i quadrati e all'interno i numeri
+play.addEventListener("click", function(){
+    grid.innerHTML = '';
+    for (let i = 1; i <= 100; i++) {
+        const square = generateSquare(i);
+        grid.append(square);
+    }
+})
+// Creo una funzione che genera un quadrato
+// Quando l'utente clicca su ogni cella, la cella cliccata si 
+// colora di azzurro ed emetto un messaggio in console 
+// con il numero della cella cliccata.
+function generateSquare(number) {
+    const square = document.createElement("div");
+    square.classList.add("ms-square-big");
+    square.classList.add("d-flex");
+    square.classList.add("justify-content-center");
+    square.classList.add("align-items-center");
+    square.innerHTML = `<span>${number}</span>`;
+    square.addEventListener('click', function(){
+        this.classList.add(`ms-bg-lightblue`);
+        alert(`Hai cliccato il numero ${number}`);
+        console.log(`Hai cliccato il numero ${number}`);
+    });
+    return square;
+}
